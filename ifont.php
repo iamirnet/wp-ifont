4219f82755dce78cfee46549807dbe7dfd976bb4
Amirhossein Jahani <me@iamir.net>
2022-02-13 07:01:25 +0330

    up

---
+++ ifont.php
@@ -0,0 +1,15 @@
+<?php
+/*
+Plugin Name: iFont
+Author URI:http://iamir.net
+Version:ver 1.0
+Plugin URI:http://iamir.net
+Description: -----
+Author:Amirhossein Jahani
+*/
+require_once("includes/functions.php");
+require_once("includes/jalali.php");
+require_once("includes/scripts.php");
+
+
+
